94e04478057da436c88e4a6f6b2cec5d55ba19c7
Odie de Moura <odie.demoura@shipsource.com>
2016-05-16 15:26:36 -0600

    Fixed problem with selecting category before entering item

--- client/features/home/home.controller.js
+++ client/features/home/home.controller.js
@@ -1,229 +1,231 @@
 /* global Firebase */
 angular.module("app").controller("homeController", function($scope, $firebaseObject, $firebaseArray, $state, userService) {
 
-	var ref = new Firebase("https://store-list.firebaseio.com/");
-	var bakery = new Firebase("https://store-list.firebaseio.com/Bakery");
-	var cannedGoods = new Firebase("https://store-list.firebaseio.com/CannedGoods");
-	var dairy = new Firebase("https://store-list.firebaseio.com/Dairy")
-	var drinks = new Firebase("https://store-list.firebaseio.com/Drinks");
+    var ref = new Firebase("https://store-list.firebaseio.com/");
+    var bakery = new Firebase("https://store-list.firebaseio.com/Bakery");
+    var cannedGoods = new Firebase("https://store-list.firebaseio.com/CannedGoods");
+    var dairy = new Firebase("https://store-list.firebaseio.com/Dairy")
+    var drinks = new Firebase("https://store-list.firebaseio.com/Drinks");
     var meat = new Firebase("https://store-list.firebaseio.com/Meat");
-	var frozen = new Firebase("https://store-list.firebaseio.com/Frozen");
-	var fruitVegetables = new Firebase("https://store-list.firebaseio.com/FruitVegetables");
-	var grains = new Firebase("https://store-list.firebaseio.com/Grains");
-	var paperCleaningProds = new Firebase("https://store-list.firebaseio.com/PaperCleaningProds");
-	var snacksChipsCandy = new Firebase("https://store-list.firebaseio.com/SnacksChipsCandy");
-	var other = new Firebase("https://store-list.firebaseio.com/Other");
+    var frozen = new Firebase("https://store-list.firebaseio.com/Frozen");
+    var fruitVegetables = new Firebase("https://store-list.firebaseio.com/FruitVegetables");
+    var grains = new Firebase("https://store-list.firebaseio.com/Grains");
+    var paperCleaningProds = new Firebase("https://store-list.firebaseio.com/PaperCleaningProds");
+    var snacksChipsCandy = new Firebase("https://store-list.firebaseio.com/SnacksChipsCandy");
+    var other = new Firebase("https://store-list.firebaseio.com/Other");
 
     var user = ref.getAuth();
 
-    if (user === null) {
-        alert("Please Login");
-        $state.go("login");
+      if (user === null) {
+          alert("Please Login");
+          $state.go("login");
     };
 
     $scope.bakery = $firebaseArray(bakery);
-	$scope.cannedGoods = $firebaseArray(cannedGoods);
-	$scope.dairy = $firebaseArray(dairy);
-	$scope.drinks = $firebaseArray(drinks);
-	$scope.meat = $firebaseArray(meat);
-	$scope.frozen = $firebaseArray(frozen);
-	$scope.fruitVegetables = $firebaseArray(fruitVegetables);
-	$scope.grains = $firebaseArray(grains);
-	$scope.paperCleaningProds = $firebaseArray(paperCleaningProds);
-	$scope.snacksChipsCandy = $firebaseArray(snacksChipsCandy);
-	$scope.other = $firebaseArray(other);
+    $scope.cannedGoods = $firebaseArray(cannedGoods);
+    $scope.dairy = $firebaseArray(dairy);
+    $scope.drinks = $firebaseArray(drinks);
+    $scope.meat = $firebaseArray(meat);
+    $scope.frozen = $firebaseArray(frozen);
+    $scope.fruitVegetables = $firebaseArray(fruitVegetables);
+    $scope.grains = $firebaseArray(grains);
+    $scope.paperCleaningProds = $firebaseArray(paperCleaningProds);
+    $scope.snacksChipsCandy = $firebaseArray(snacksChipsCandy);
+    $scope.other = $firebaseArray(other);
 
-	$scope.dropDown = "Select Category";
+    $scope.dropDown = "Select Category";
     $scope.newItem = "";
 
     $scope.addItem = function(){
 
-        var newItem = $scope.newItem.trim();
-        if (!newItem.length) {
+      var newItem = $scope.newItem.trim();
+      if (!newItem.length) {
 
-			alert("Please enter item");
-            return;
-        }
-
-		if ($scope.dropDown === "Bakery") {
-
-			$scope.bakery.$add({
-
-				title: newItem,
-				category: "bakery",
-				completed: false
-			});
-
-		}
-
-		if ($scope.dropDown === "Canned Goods") {
-
-			$scope.cannedGoods.$add({
-
-				title: newItem,
-				category: "canned goods",
-				completed: false
-			});
-
-		}
-
-		if ($scope.dropDown === "Dairy") {
-
-			$scope.dairy.$add({
-
-				title: newItem,
-				category: "dairy",
-				completed: false
-			});
-		}
-
-		if ($scope.dropDown === "Drinks") {
-
-			$scope.drinks.$add({
-
-				title: newItem,
-				category: "drinks",
-				completed: false
-			});
-		}
-
-		if ($scope.dropDown === "Meat") {
-
-			$scope.meat.$add({
-
-				title: newItem,
-				category: "meat",
-				completed: false
-			});
-
-		}
-
-		if ($scope.dropDown === "Frozen") {
-
-			$scope.frozen.$add({
-
-				title: newItem,
-				category: "frozen",
-				completed: false
-			});
-		}
-
-		if ($scope.dropDown === "Fruit/Vegetables") {
-
-			$scope.fruitVegetables.$add({
-
-				title: newItem,
-				category: "fruitVegetables",
-				completed: false
-			});
-
-		}
-
-		if ($scope.dropDown === "Grains") {
-
-			$scope.grains.$add({
-
-				title: newItem,
-				category: "grains",
-				completed: false
-			});
-
-		}
-
-		if ($scope.dropDown === "Paper/CleaningProds") {
-
-			$scope.paperCleaningProds.$add({
-
-				title: newItem,
-				category: "paperCleaningProds",
-				completed: false
-			});
-		}
-
-		if ($scope.dropDown === "Snacks/Chips/Candy") {
-
-			$scope.snacksChipsCandy.$add({
-
-				title: newItem,
-				category: "snacksChipsCandy",
-				completed: false
-			});
-
-		}
-
-		if ($scope.dropDown === "Other") {
-
-			$scope.other.$add({
-
-				title: newItem,
-				category: "other",
-				completed: false
-			});
-		}
-
-		$scope.dropDown = "Select Category";
+        alert("Please enter item");
+        $scope.dropDown = "Select Category";
         $scope.newItem = "";
-    };
+        return;
+      }
 
-    $scope.removeItem = function(item){
+      if ($scope.dropDown === "Bakery") {
 
-		if (item.category === "bakery") {
+        $scope.bakery.$add({
 
-			$scope.bakery.$remove(item);
-		}
+          title: newItem,
+          category: "bakery",
+          completed: false
+        });
 
-		if (item.category === "canned goods") {
+      }
 
-			$scope.cannedGoods.$remove(item);
-		}
+      if ($scope.dropDown === "Canned Goods") {
 
-		if (item.category === "dairy") {
+        $scope.cannedGoods.$add({
 
-			$scope.dairy.$remove(item);
-		}
+          title: newItem,
+          category: "canned goods",
+          completed: false
+        });
 
-		if (item.category === "drinks") {
+      }
 
-			$scope.drinks.$remove(item);
-		}
+      if ($scope.dropDown === "Dairy") {
 
-		if (item.category === "meat") {
+        $scope.dairy.$add({
 
-			$scope.meat.$remove(item);
-		}
+          title: newItem,
+          category: "dairy",
+          completed: false
+        });
+      }
 
-		if (item.category === "frozen") {
+      if ($scope.dropDown === "Drinks") {
 
-			$scope.frozen.$remove(item);
-		}
+        $scope.drinks.$add({
 
-		if (item.category === "fruitVegetables") {
+          title: newItem,
+          category: "drinks",
+          completed: false
+        });
+      }
 
-			$scope.fruitVegetables.$remove(item);
-		}
+      if ($scope.dropDown === "Meat") {
 
-		if (item.category === "grains") {
+        $scope.meat.$add({
 
-			$scope.grains.$remove(item);
-		}
+          title: newItem,
+          category: "meat",
+          completed: false
+        });
 
-		if (item.category ==="paperCleaningProds") {
+      }
 
-			$scope.paperCleaningProds.$remove(item);
-		}
+      if ($scope.dropDown === "Frozen") {
 
-		if (item.category ==="snacksChipsCandy") {
+        $scope.frozen.$add({
 
-			$scope.snacksChipsCandy.$remove(item);
-		}
+          title: newItem,
+          category: "frozen",
+          completed: false
+        });
+      }
 
-		if (item.category ==="other") {
+      if ($scope.dropDown === "Fruit/Vegetables") {
 
-			$scope.other.$remove(item);
-		}
-    };
+        $scope.fruitVegetables.$add({
 
-    $scope.logout = function() {
-        userService.logout(ref);
-    };
-});
+          title: newItem,
+          category: "fruitVegetables",
+          completed: false
+        });
+
+      }
+
+      if ($scope.dropDown === "Grains") {
+
+        $scope.grains.$add({
+
+          title: newItem,
+          category: "grains",
+          completed: false
+        });
+
+      }
+
+      if ($scope.dropDown === "Paper/CleaningProds") {
+
+        $scope.paperCleaningProds.$add({
+
+          title: newItem,
+          category: "paperCleaningProds",
+          completed: false
+        });
+      }
+
+      if ($scope.dropDown === "Snacks/Chips/Candy") {
+
+        $scope.snacksChipsCandy.$add({
+
+          title: newItem,
+          category: "snacksChipsCandy",
+          completed: false
+        });
+
+      }
+
+      if ($scope.dropDown === "Other") {
+
+        $scope.other.$add({
+
+          title: newItem,
+          category: "other",
+          completed: false
+        });
+      }
+
+      $scope.dropDown = "Select Category";
+          $scope.newItem = "";
+      };
+
+      $scope.removeItem = function(item){
+
+      if (item.category === "bakery") {
+
+        $scope.bakery.$remove(item);
+      }
+
+      if (item.category === "canned goods") {
+
+        $scope.cannedGoods.$remove(item);
+      }
+
+      if (item.category === "dairy") {
+
+        $scope.dairy.$remove(item);
+      }
+
+      if (item.category === "drinks") {
+
+        $scope.drinks.$remove(item);
+      }
+
+      if (item.category === "meat") {
+
+        $scope.meat.$remove(item);
+      }
+
+      if (item.category === "frozen") {
+
+        $scope.frozen.$remove(item);
+      }
+
+      if (item.category === "fruitVegetables") {
+
+        $scope.fruitVegetables.$remove(item);
+      }
+
+      if (item.category === "grains") {
+
+        $scope.grains.$remove(item);
+      }
+
+      if (item.category ==="paperCleaningProds") {
+
+        $scope.paperCleaningProds.$remove(item);
+      }
+
+      if (item.category ==="snacksChipsCandy") {
+
+        $scope.snacksChipsCandy.$remove(item);
+      }
+
+      if (item.category ==="other") {
+
+        $scope.other.$remove(item);
+      }
+      };
+
+      $scope.logout = function() {
+          userService.logout(ref);
+      };
+  });
